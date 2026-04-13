80d84852e2253cd5601717ce41b0bff152158576
EATTHEGOD <miha.vasilev08@yandex.ru>
2026-04-13 18:54:21 +0300

    ui: tweak bell + sign-in sizing

--- src/components/GoogleMockPage.tsx
+++ src/components/GoogleMockPage.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 function BellIcon() {
   return (
-    <svg viewBox="0 0 24 24" width="22" height="22" aria-hidden="true">
+    <svg viewBox="0 0 24 24" width="20" height="20" aria-hidden="true">
       <path d="M12 22a2 2 0 0 0 2-2H10a2 2 0 0 0 2 2Z" fill="currentColor" />
       <path
         d="M18 16V11a6 6 0 1 0-12 0v5l-2 2h16l-2-2Z"
